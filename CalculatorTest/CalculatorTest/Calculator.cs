f81103b13a563d5d0eda874ac70957859a8f7c0a
Ryan Smith <Rwsmith8692@gmail.com>
2019-09-10 08:38:43 -0500

    CalculatorTest

---
+++ CalculatorTest/CalculatorTest/Calculator.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+
+namespace CalculatorTest
+{
+    public class Calculator
+    {
+        public int Adding(int num1, int num2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Subtracting(int num1, int num2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Multiplying(int num1, int num2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Dividing(int num1, int num2)
+        {
+            throw new NotImplementedException();
+        }
+
+        public int Factorial(int num1)
+        {
+            throw new NotImplementedException();
+        }
+    }
+
+  
+    
+        
+    
+}
